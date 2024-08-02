5b88b435480b9430189bfb42e42f81df8e73b6b2
Mateus Elias <meap@cin.ufpe.br>
2024-08-02 16:52:06 -0300

    fix find or create in Zipcode::Service::Address.emerge() method

--- app/services/zipcode/service/address.rb
+++ app/services/zipcode/service/address.rb
@@ -6,11 +6,11 @@ module Zipcode
       def self.emerge(zipcode)
         address = Zipcode::Api::Address.find(zipcode)
 
-        ::Address.find_or_create_by(zipcode: zipcode) do
-          _1.street = address.street,
-          _1.state = address.state,
-          _1.city = address.city,
-          _1.neighborhood = address.neighborhood,
+        ::Address.find_or_create_by(zipcode: address.zipcode) do
+          _1.street = address.street
+          _1.state = address.state
+          _1.city = address.city
+          _1.neighborhood = address.neighborhood
           _1.areacode = address.areacode
           _1.zipcode = address.zipcode
         end
