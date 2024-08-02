14190b6fa0952155619e67e78dbc9872b2bf5cb9
Mateus Elias <meap@cin.ufpe.br>
2024-08-02 16:14:32 -0300

    update Zipcode::Service::Address.emerge() method

--- app/services/zipcode/service/address.rb
+++ app/services/zipcode/service/address.rb
@@ -6,13 +6,13 @@ module Zipcode
       def self.emerge(zipcode)
         address = Zipcode::Api::Address.find(zipcode)
 
-        ::Address.find_or_create_by(
-          street: address.street,
-          state: address.state,
-          city: address.city,
-          neighborhood: address.neighborhood,
-          areacode: address.areacode
-        )
+        ::Address.find_or_create_by(zipcode: zipcode) do
+          _1.street = address.street,
+          _1.state = address.state,
+          _1.city = address.city,
+          _1.neighborhood = address.neighborhood,
+          _1.areacode = address.areacode
+        end
       end
     end
   end
