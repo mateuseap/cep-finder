971ab4c12c14d102d87dc757c2f22e03a09d049e
Mateus Elias <meap@cin.ufpe.br>
2024-08-15 19:53:11 -0300

    update parse() method of Zipcode::Client to use @options to check parse type

--- app/services/zipcode/client.rb
+++ app/services/zipcode/client.rb
@@ -3,11 +3,11 @@
 module Zipcode
   class Client < MultiTools::ApiClient
     private
-      def parse(response, xml: true)
+      def parse(response)
         if !valid?(response)
           raise MultiTools::ApiClient::Error.new "#{@logname}: Invalid response received - #{log_sanitizer(response.body)}", log_sanitizer(response.body), response.code
         elsif response.body.present?
-          if xml
+          if @options[:xml]
             Hash.from_xml(response.body)
           else
             JSON.parse(response.body)
