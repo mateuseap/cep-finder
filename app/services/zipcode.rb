71496c70127624bab35f09ecd5b8a1b0d1c336aa
Mateus Elias <meap@cin.ufpe.br>
2024-08-15 19:52:04 -0300

    modify configurations and some methods of Zipcode integration

--- app/services/zipcode.rb
+++ app/services/zipcode.rb
@@ -2,20 +2,20 @@
 
 module Zipcode
   DEFAULTS = {
-    endpoint: "https://cep.awesomeapi.com.br/xml/",
+    endpoint: "https://cep.awesomeapi.com.br",
   }
 
   def self.options
     DEFAULTS.dup
   end
 
-  def self.client
-    Zipcode::Client.new(options)
+  def self.client(xml: false)
+    Zipcode::Client.new(options.merge(xml: xml))
   end
 
   # Import addresses from a list of zipcodes
   def self.import_addresses(zipcodes)
-    zipcodes.each do |zipcode|
+    zipcodes.map do |zipcode|
       Zipcode::Service::Address.emerge(zipcode)
     end
   end
